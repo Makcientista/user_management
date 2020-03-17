c95562aa6973cd82d55bf714d734be1d4cfede58
makiesse <ddociel@hotmail.com>
2020-03-17 02:15:59 -0300

    creating duplicate login validation

---
+++ src/servlet/Usuario.java
@@ -0,0 +1,118 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.Spring;
+import beans.BeanCursoJsp;
+import dao.DaoUsuario;
+
+@MultipartConfig
+@WebServlet("/salvarUsuario")
+
+public class Usuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private DaoUsuario daoUsuario = new DaoUsuario();
+
+	public Usuario() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		try {
+			String acao = request.getParameter("acao");
+			String user = request.getParameter("user");
+
+			if (acao.equalsIgnoreCase("delete")) {
+				daoUsuario.delete(user);
+				RequestDispatcher view = request.getRequestDispatcher("cadastroUsuario.jsp");
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response);
+			}
+
+			else if (acao.equalsIgnoreCase("editar")) {
+				BeanCursoJsp beanCursoJsp = daoUsuario.consultar(user);
+				RequestDispatcher view = request.getRequestDispatcher("cadastroUsuario.jsp");
+				request.setAttribute("user", beanCursoJsp);
+				view.forward(request, response);
+			} else if (acao.equalsIgnoreCase("listartodos")) {
+
+				RequestDispatcher view = request.getRequestDispatcher("cadastroUsuario.jsp");
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String acao = request.getParameter("acao");
+
+		if (acao != null && acao.equalsIgnoreCase("reset")) {
+
+			try {
+				RequestDispatcher view = request.getRequestDispatcher("/cadastroUsuario.jsp");
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} else {
+
+			String id = request.getParameter("id");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+			String nome = request.getParameter("nome");
+			String telefone = request.getParameter("telefone");
+		
+
+			BeanCursoJsp usuario = new BeanCursoJsp();
+			usuario.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : 0);
+			usuario.setLogin(login);
+			usuario.setSenha(senha);
+			usuario.setNome(nome);
+			usuario.setTelefone(telefone);
+			
+			try {
+				
+				if(id == null || id.isEmpty() && !daoUsuario.validarLogin(login)) {
+					request.setAttribute("msg", "Já existe um usuário com este login!");
+				}
+
+				if (id == null || id.isEmpty() && daoUsuario.validarLogin(login)) {
+					
+					daoUsuario.salvar(usuario);
+					
+				} else if(id != null && !id.isEmpty()) {
+					daoUsuario.atualizar(usuario);
+				}
+
+				// para ficar na mesma pagina ap�s cadastrar novo usu�rio:
+
+				RequestDispatcher view = request.getRequestDispatcher("/cadastroUsuario.jsp");
+				request.setAttribute("usuarios", daoUsuario.listar());
+				view.forward(request, response);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
